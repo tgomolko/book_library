96b4eb4731b0bc1fe14c8ba19632701902e8b22f
Timofey Gomolko <t.i.m.a2009@mail.ru>
2020-04-07 18:18:27 +0300

    fix js connection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,51 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require control
+//= require_tree .
+
+$(document).ready (function () {
+   $(document).on("mouseenter", ".dropdown", function(){
+    $(this).toggleClass("is-active");
+  });
+});
+
+$(document).ready (function () {
+   $(document).on("mouseleave", ".dropdown", function(){
+    $(this).toggleClass("is-active");
+  });
+});
+
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
+
+$(document).ready(function() {
+  $("#add").click(function() {
+    $("#add-card").toggleClass("active");
+  });
+});
+
+$(document).ready (function () {
+   $(document).on("click", "#del", function(){
+    $(".modal").toggleClass("is-active");
+  });
+});
+
+$(document).ready (function () {
+   $(document).on("click", ".modal-background", function(){
+    $(".modal").toggleClass("is-active");
+  });
+});
+
+$(document).ready(function () {
+  $(document).keydown(function(e){
+    if(e.keyCode == 27) {
+      $(".modal").toggleClass("is-active");
+    }
+  });
+});
